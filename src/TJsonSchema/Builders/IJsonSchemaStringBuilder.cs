910aea5a23063c83c2211eeeef38d58a0fed6519
touya187842 <touya187842@yahoo.co.jp>
2024-11-30 12:46:56 +0800

    doc for IJsonSchemaStringBuilder

--- src/TJsonSchema/Builders/IJsonSchemaStringBuilder.cs
+++ src/TJsonSchema/Builders/IJsonSchemaStringBuilder.cs
@@ -2,13 +2,28 @@ using System.Text.RegularExpressions;
 
 namespace TJsonSchema.Builders;
 
+/// <summary>
+/// 表示一個關於 字串 的 JsonSchema 建造者
+/// </summary>
 public interface IJsonSchemaStringBuilder : IJsonSchemaRootBuilder
 {
+    /// <summary>
+    /// 設定字串須符合已知的 format
+    /// </summary>
     internal JsonSchemaStringFormat? Format { get; set; }
 
+    /// <summary>
+    /// 設定字串須符合 Regex
+    /// </summary>
     internal Regex? Pattern { get; set; }
 
+    /// <summary>
+    /// 設定字串的最小長度
+    /// </summary>
     internal int? MinLength { get; set; }
 
+    /// <summary>
+    /// 設定字串的最大長度
+    /// </summary>
     internal int? MaxLength { get; set; }
 }
