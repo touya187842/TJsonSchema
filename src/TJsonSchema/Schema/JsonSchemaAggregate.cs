db7d6a3030df763a06caeec7eaa5e84db8914132
touya187842 <touya187842@yahoo.co.jp>
2024-12-05 22:11:33 +0800

    add JsonSchema base helpers

---
+++ src/TJsonSchema/Schema/JsonSchemaAggregate.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TJsonSchema.Schema;
+
+internal class JsonSchemaAggregate<T> : JsonSchemaBase<T> where T : IJsonNode
+{
+    private readonly IEnumerable<IJsonSchema<T>> Schemas;
+
+    protected internal JsonSchemaAggregate(IEnumerable<IJsonSchema<T>> schemas)
+    {
+        Schemas = schemas;
+    }
+
+    public override bool Validate(T value)=> Schemas.Any(schema => schema.Validate(value));
+}
---
+++ src/TJsonSchema/Schema/JsonSchemaBase.cs
@@ -0,0 +1,11 @@
+namespace TJsonSchema.Schema;
+
+internal abstract class JsonSchemaBase<T> : IJsonSchema<T> where T : IJsonNode
+{
+    public bool Validate(IJsonNode node)
+    {
+        return node is T typed && Validate(typed);
+    }
+
+    public abstract bool Validate(T value);
+}
