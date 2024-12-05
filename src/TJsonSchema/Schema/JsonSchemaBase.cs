973c20f3ce377f61cd5790d640228f74c8d2cbc0
touya187842 <touya187842@yahoo.co.jp>
2024-12-05 23:41:46 +0800

    fix contract nullability

--- src/TJsonSchema/Schema/JsonSchemaBase.cs
+++ src/TJsonSchema/Schema/JsonSchemaBase.cs
@@ -2,7 +2,7 @@ namespace TJsonSchema.Schema;
 
 internal abstract class JsonSchemaBase<T> : IJsonSchema<T> where T : IJsonNode
 {
-    public bool Validate(IJsonNode node)
+    public bool Validate(IJsonNode? node)
     {
         return node is T typed && Validate(typed);
     }
